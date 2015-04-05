e0b074f5fa5e56206469ddfe348d4fd6204802ab
cperez3 <chrisv.perez@gmail.com>
2015-04-05 14:07:02 -0400

    things

--- summoner.py
+++ summoner.py
@@ -4,7 +4,7 @@ import os
 import sys
 from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship
+from sqlalchemy.orm import relationship, sessionmaker
 from sqlalchemy import create_engine
 
 Base = declarative_base()
@@ -14,3 +14,27 @@ class Summoner(Base):
 
     id = Column(Integer, primary_key=True)
     name = Column(String(20), nullable=False)
+    profileIconId = Column(Integer)
+    revisionDate = Column(Integer)
+    summonerLevel = Column(Integer)
+
+
+
+# Create an engine that stores data in the local directory's
+# sqlalchemy_example.db file.
+engine = create_engine("data.db")
+
+# A DBSession() instance establishes all conversations with the database
+# and represents a "staging zone" for all the objects loaded into the
+# database session object. Any change made against the objects in the
+# session won't be persisted into the database until you call
+# session.commit(). If you're not happy about the changes, you can
+# revert all of them back to the last commit by calling
+# session.rollback()
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
+
+# Create all tables in the engine. This is equivalent to "Create Table"
+# statements in raw SQL.
+Base.metadata.create_all(engine)
