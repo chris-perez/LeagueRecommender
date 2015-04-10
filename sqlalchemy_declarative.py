5a00e6257ccb7e7d8f520956fa31020b7d071d99
Nick <nicksbpittman@gmail.com>
2015-04-10 12:52:05 -0400

    more things

--- sqlalchemy_declarative.py
+++ sqlalchemy_declarative.py
@@ -22,13 +22,24 @@ class Match(Base):
     __tablename__ = 'match'
     id = Column(Integer, primary_key=True)
 
-
 class SummonerToMatch(Base):
     __tablename__ = 'summoner_to_match'
     id = Column(Integer, primary_key=True)
     summonerId = Column(Integer)
     name = Column(String(20), nullable=False)#not sure how if you want to use summonerID or name
     matchId = Column(Integer)
+    championId = Column(Integer)
+    previousRank = Column(String(20))
+    summonerSpell1 = Column(Integer)
+    summonerSpell2 = Column(Integer)
+    kills = Column(Integer)
+    deaths = Column(Integer)
+    assists = Column(Integer)
+    champLevel = Column(Integer)
+    firstBloodKill = Column(String(5))
+    goldEarned = Column(Integer)
+    towerKills = Column(Integer)
+    winner = (String(5))
 
 class SummonerToChampion(Base):
     __tablename__ = 'summoner_to_champion'
@@ -36,7 +47,11 @@ class SummonerToChampion(Base):
     summonerId = Column(Integer)
     name = Column(String(20), nullable=False)
     championId = Column(Integer)
-
+    kills = Column(Integer)
+    deaths = Column(Integer)
+    assists = Column(Integer)
+    wins = Column(Integer)
+    games = Column(Integer)
 
 
 # Create an engine that stores data in the local directory's
